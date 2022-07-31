9816cd115fd89f8a2a3e3bfb47594be518aaad11
Johnson2772 <3270380+Johnson2772@users.noreply.github.com>
2022-07-31 05:21:07 -0400

    Fixing errors... Debug Pending

--- Bash/Linux Mint Setup.sh
+++ Bash/Linux Mint Setup.sh
@@ -124,34 +124,35 @@ echo "[*] [ $progress/$total ] Installing x2go"
 sudo apt install -y x2goclient && ((progress++)) && echo "[*] [ $progress/$total ] x2go Installed"
 
 # Installing Capitaine Icon Theme & Cursor
-icons=~/.icons
+icons=$HOME/.icons
 capitaine=$icons/la-capitaine-icon-theme
 capitaineCursor=$icons/capitaine-cursors
 if [ ! -f "$capitaine" ] ; then
-    # Download Theme & Cursor
+    # Downloading Theme & Cursor
     cd $icons || exit
     git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git la-capitaine-icon-theme
     git clone https://github.com/keeferrourke/capitaine-cursors.git capitaine-cursors
     # Installing Theme
-    cd $capitaine || exit
-    chmod u+x .configure
+    cd "$capitaine" || exit
+    chmod u+x configure
     ./configure
     # Installing Cursor
-    cd $capitaineCursor || exit
+    cd "$capitaineCursor" || exit
     chmod u+x .build
     ./build.sh
     cd $apps || exit
     ((progress++))
-    echo "[*] [$progress/$total] Capitaine Icon Theme and Cursor Installed"
+    echo "[*] [$progress/$total] Capitaine Icon Theme & Cursor Installed"
 else
     # Installing Theme
-    cd $capitaine || exit
-    chmod u+x .configure
+    cd "$capitaine" || exit
+    chmod u+x configure
+    ./configure
     # Installing Cursor
-    cd $capitaineCursor || exit
+    cd "$capitaineCursor" || exit
     chmod u+x .build
     ./build.sh
     cd $apps || exit
     ((progress++))
-    echo "[*] [$progress/$total] Capitaine Icon Theme Installed"
+    echo "[*] [$progress/$total] Capitaine Icon Theme & Cursor Installed"
 fi
