ec869565f01e0dded809fe5e832ec8ffe67a6abf
Jhonatan Posada <3270380+Johnson2772@users.noreply.github.com>
2022-08-12 02:09:55 -0400

    Organizing software

--- bash/install-software.sh
+++ bash/install-software.sh
@@ -6,7 +6,7 @@
 bash="at atop htop lynx ninvaders sl xfonts-terminus"
 devTools="anjuta bluefish eclipse emacs geany meld netbeans ruby samba vim virtualbox"
 emulators="atari800 desmume dolphin-emu dosbox gngb hatari mame mednafen nestopia pcsxr playonlinux scummvm stella visualboyadvance zsnes"
-extras="cheese copyq filezilla flameshot gparted grsync hardinfo k3b librecad libreoffice pl plank qalculate recordmydesktop sysinfo vinagre vino vlc xscreensaver xscreensaver-data-extra xscreensaver-gl-extra"
+extras="audacity cheese copyq filezilla flameshot gparted grsync hardinfo k3b librecad libreoffice pl plank qalculate software-properties-common recordmydesktop sysinfo vinagre vino vlc xscreensaver xscreensaver-data-extra xscreensaver-gl-extra"
 games="0ad alien-arena assaultcube chessx enigma hedgewars lbreakout2 minetest neverball nexuiz nexuiz-server openarena openarena-081-maps openarena-081-players openarena-081-players-mature openarena-081-textures openarena-server sauerbraten steam:i386 wesnoth xmoto"
 gamesExtra="armagetronad billard-gl blobwars boswars circuslinux dreamchess extremetuxracer pingus pychess singularity snake4 supertux supertuxkart teeworlds warzone2100 xpenguins"
 graphicTools="dia gcolor3 gimp gpick inkscape krita scribus scribus-template"
@@ -19,6 +19,99 @@ everything="$bash $devTools $emulators $extras $games $gamesExtra $graphicTools 
 # Updating system
 apt-get update
 apt-get upgrade
+
+# Installs Cerebro from deb package
+echo "[*] [$progress/$total] Installing cerebro"
+cerebro="cerebro.deb"
+if [ ! -f "$apps/$cerebro" ] ; then
+    wget -q -O $apps/$cerebro https://github.com/cerebroapp/cerebro/releases/download/v0.5.0/cerebro_0.5.0_amd64.deb
+    dpkg -i $apps/$cerebro
+    ((progress++))
+    echo "[*] [$progress/$total] Cerebro Installed"
+else
+    dpkg -i $apps/$cerebro
+    ((progress++))
+    echo "[*] [$progress/$total] Cerebro Installed"
+	rm $apps/$cerebro
+fi
+
+# Installing Software from repositories.
+# aircrack
+echo "[*] [ $progress/$total ] Installing aircrack-ng"
+apt install -y aircrack-ng && ((progress++)) && echo "[*] [ $progress/$total ] aircrack Installed"
+# CIFS
+echo "[*] [ $progress/$total ] Installing CIFS Utils"
+sudo apt install -y cifs-utils && ((progress++)) && echo "[*] [ $progress/$total ] CIFS Installed"
+# Darktable
+echo "[*] [ $progress/$total ] Installing darktable"
+sudo add-apt-repository -y ppa:pmjdebruijn/darktable-release
+sudo apt update
+sudo apt install -y darktable && ((progress++)) && echo "[*] [ $progress/$total ] darktable Installed"
+# Emacs
+echo "[*] [ $progress/$total ] Installing Emacs"
+sudo apt install -y emacs && ((progress++)) && echo "[*] [ $progress/$total ] Emacs Installed"
+# exfat
+echo "[*] [ $progress/$total ] Installing Exfat Utils"
+sudo apt install -y exfat-utils && ((progress++)) && echo "[*] [ $progress/$total ] exfat-utils Installed"
+# Gitg
+echo "[*] [ $progress/$total ] Installing gitg"
+sudo apt install -y gitg && ((progress++)) && echo "[*] [ $progress/$total ] Gitg Installed"
+# Glances
+echo "[*] [ $progress/$total ] Installing glances"
+sudo apt install -y glances && ((progress++)) && echo "[*] [ $progress/$total ] glances Installed"
+# iftop
+echo "[*] [ $progress/$total ] Installing iftop"
+sudo apt install -y iftop && ((progress++)) && echo "[*] [ $progress/$total ] iftop Installed"
+# Inkscape
+echo "[*] [ $progress/$total ] Installing Inkscape"
+sudo apt install -y inkscape && ((progress++)) && echo "[*] [ $progress/$total ] Inkscape Installed"
+# iotop
+echo "[*] [ $progress/$total ] Installing iotop"
+sudo apt install -y iotop && ((progress++)) && echo "[*] [ $progress/$total ] iotop Installed"
+# LibreOffice Styles
+echo "[*] [ $progress/$total ] Installing LibreOffice Styles"
+sudo apt install -y libreoffice-style-sifr && ((progress++)) && echo "[*] [ $progress/$total ] LibreOffice Styles Installed"
+# lm-sensors
+echo "[*] [ $progress/$total ] Installing lm-sensors"
+sudo apt install -y lm-sensors && ((progress++)) && echo "[*] [ $progress/$total ] lm-sensors Installed"
+# nmap
+echo "[*] [ $progress/$total ] Installing nmap"
+sudo apt install -y nmap && ((progress++)) && echo "[*] [ $progress/$total ] nmap Installed"
+# npm
+echo "[*] [ $progress/$total ] Installing nmp"
+sudo apt install -y nmp && ((progress++)) && echo "[*] [ $progress/$total ] nmp Installed"
+# OpenVPN
+echo "[*] [ $progress/$total ] Installing OpenVPN"
+sudo apt install -y openvpn && ((progress++)) && echo "[*] [ $progress/$total ] OpenVPN Installed"
+# Plank
+echo "[*] [ $progress/$total ] Installing Plank"
+sudo apt install -y plank && ((progress++)) && echo "[*] [ $progress/$total ] Plank Installed"
+# Remmina
+echo "[*] [ $progress/$total ] Installing remmina"
+sudo apt install -y remmina && ((progress++)) && echo "[*] [ $progress/$total ] Remmina Installed"
+# Stacer
+echo "[*] [ $progress/$total ] Installing Stacer"
+sudo apt install -y stacer && ((progress++)) && echo "[*] [ $progress/$total ] stacer Installed"
+# Tmux
+echo "[*] [ $progress/$total ] Installing tmux"
+sudo apt install -y tmux && ((progress++)) && echo "[*] [ $progress/$total ] Tmux Installed"
+# Veracrypt
+echo "[*] [ $progress/$total ] Installing Veracrypt"
+sudo add-apt-repository -y ppa:unit193/encryption
+sudo apt update
+sudo apt install -y veracrypt && ((progress++)) && echo "[*] [ $progress/$total ] Veracrypt Installed"
+# VLC
+echo "[*] [ $progress/$total ] Installing VLC"
+sudo apt install -y vlc && ((progress++)) && echo "[*] [ $progress/$total ] VLC Installed"
+# WireShark
+echo "[*] [ $progress/$total ] Installing WireShark"
+sudo apt install -y wireshark && ((progress++)) && echo "[*] [ $progress/$total ] WireShark Installed"
+# x2go
+echo "[*] [ $progress/$total ] Installing x2go"
+sudo apt install -y x2goclient && ((progress++)) && echo "[*] [ $progress/$total ] x2go Installed"
+
+
+
 
 # Installing some useful PPA
 # Visit website for more information http://www.webupd8.org/
--- bash/linux-mint-setup.sh
+++ bash/linux-mint-setup.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 # Setting up my Linux Desktop Environment.
-# Declaring some variables.
-apps="/tmp/apps"
-progress=1
-total=50
-
-# Creating directory if doesn't exist.
-cd "$(dirname "$0")" || exit 
-if [ ! -d "$apps" ]; then
-	mkdir "$apps"
-fi
-
 # Updating and Upgrading Linux Mint System.
 # Installing it necessary software to begin with.
 echo "Hello World!"
@@ -30,103 +19,8 @@ sudo mintupgrade
 apt-get dist-upgrade && apt update && apt upgrade && ((progress++))
 echo "[*] [$progress/$total] Finishing Upgrade"
 
-# Installs Cerebro from deb package
-echo "[*] [$progress/$total] Installing cerebro"
-cerebro="cerebro.deb"
-if [ ! -f "$apps/$cerebro" ] ; then
-    wget -q -O $apps/$cerebro https://github.com/cerebroapp/cerebro/releases/download/v0.5.0/cerebro_0.5.0_amd64.deb
-    dpkg -i $apps/$cerebro
-    ((progress++))
-    echo "[*] [$progress/$total] Cerebro Installed"
-else
-    dpkg -i $apps/$cerebro
-    ((progress++))
-    echo "[*] [$progress/$total] Cerebro Installed"
-	rm $apps/$cerebro
-fi
-
-# Installing Software from repositories.
-# Extras
-echo "[*] [ $progress/$total ] Installing Extras"
-sudo apt install -y software-properties-common && ((progress++)) && echo "[*] [ $progress/$total ] Installed software-properties-common"
-# aircrack
-echo "[*] [ $progress/$total ] Installing aircrack-ng"
-apt install -y aircrack-ng && ((progress++)) && echo "[*] [ $progress/$total ] aircrack Installed"
-# audacity
-echo "[*] [ $progress/$total ] Installing audacity"
-sudo apt install -y audacity && ((progress++)) && echo "[*] [ $progress/$total ] audacity Installed"
-# CIFS
-echo "[*] [ $progress/$total ] Installing samba tools"
-sudo apt install -y cifs-utils && ((progress++)) && echo "[*] [ $progress/$total ] CIFS Installed"
-# Darktable
-echo "[*] [ $progress/$total ] Installing darktable"
-sudo add-apt-repository -y ppa:pmjdebruijn/darktable-release
-sudo apt update
-sudo apt install -y darktable && ((progress++)) && echo "[*] [ $progress/$total ] darktable Installed"
-# Emacs
-echo "[*] [ $progress/$total ] Installing Emacs"
-sudo apt install -y emacs && ((progress++)) && echo "[*] [ $progress/$total ] Emacs Installed"
-# exfat
-echo "[*] [ $progress/$total ] Installing exfat-utils"
-sudo apt install -y exfat-utils && ((progress++)) && echo "[*] [ $progress/$total ] exfat-utils Installed"
-# Gitg
-echo "[*] [ $progress/$total ] Installing gitg"
-sudo apt install -y gitg && ((progress++)) && echo "[*] [ $progress/$total ] Gitg Installed"
-# Glances
-echo "[*] [ $progress/$total ] Installing glances"
-sudo apt install -y glances && ((progress++)) && echo "[*] [ $progress/$total ] glances Installed"
-# iftop
-echo "[*] [ $progress/$total ] Installing iftop"
-sudo apt install -y iftop && ((progress++)) && echo "[*] [ $progress/$total ] iftop Installed"
-# Inkscape
-echo "[*] [ $progress/$total ] Installing Inkscape"
-sudo apt install -y inkscape && ((progress++)) && echo "[*] [ $progress/$total ] Inkscape Installed"
-# iotop
-echo "[*] [ $progress/$total ] Installing iotop"
-sudo apt install -y iotop && ((progress++)) && echo "[*] [ $progress/$total ] iotop Installed"
-# LibreOffice Styles
-echo "[*] [ $progress/$total ] Installing LibreOffice Styles"
-sudo apt install -y libreoffice-style-sifr && ((progress++)) && echo "[*] [ $progress/$total ] LibreOffice Styles Installed"
-# lm-sensors
-echo "[*] [ $progress/$total ] Installing lm-sensors"
-sudo apt install -y lm-sensors && ((progress++)) && echo "[*] [ $progress/$total ] lm-sensors Installed"
-# nmap
-echo "[*] [ $progress/$total ] Installing nmap"
-sudo apt install -y nmap && ((progress++)) && echo "[*] [ $progress/$total ] nmap Installed"
-# npm
-echo "[*] [ $progress/$total ] Installing nmp"
-sudo apt install -y nmp && ((progress++)) && echo "[*] [ $progress/$total ] nmp Installed"
-# OpenVPN
-echo "[*] [ $progress/$total ] Installing OpenVPN"
-sudo apt install -y openvpn && ((progress++)) && echo "[*] [ $progress/$total ] OpenVPN Installed"
-# Plank
-echo "[*] [ $progress/$total ] Installing Plank"
-sudo apt install -y plank && ((progress++)) && echo "[*] [ $progress/$total ] Plank Installed"
-# Remmina
-echo "[*] [ $progress/$total ] Installing remmina"
-sudo apt install -y remmina && ((progress++)) && echo "[*] [ $progress/$total ] Remmina Installed"
-# Stacer
-echo "[*] [ $progress/$total ] Installing Stacer"
-sudo apt install -y stacer && ((progress++)) && echo "[*] [ $progress/$total ] stacer Installed"
-# Tmux
-echo "[*] [ $progress/$total ] Installing tmux"
-sudo apt install -y tmux && ((progress++)) && echo "[*] [ $progress/$total ] Tmux Installed"
-# Veracrypt
-echo "[*] [ $progress/$total ] Installing Veracrypt"
-sudo add-apt-repository -y ppa:unit193/encryption
-sudo apt update
-sudo apt install -y veracrypt && ((progress++)) && echo "[*] [ $progress/$total ] Veracrypt Installed"
-# VLC
-echo "[*] [ $progress/$total ] Installing VLC"
-sudo apt install -y vlc && ((progress++)) && echo "[*] [ $progress/$total ] VLC Installed"
-# WireShark
-echo "[*] [ $progress/$total ] Installing WireShark"
-sudo apt install -y wireshark && ((progress++)) && echo "[*] [ $progress/$total ] WireShark Installed"
-# x2go
-echo "[*] [ $progress/$total ] Installing x2go"
-sudo apt install -y x2goclient && ((progress++)) && echo "[*] [ $progress/$total ] x2go Installed"
-
 # Install new HAL9000 cow
+echo "[*] [ $progress/$total ] Installing HAL9000"
 wget -q -O HAL9000.cow https://github.com/Johnson2772/scripts/tree/main/bash/preferences/HAL9000/HAL9000.cow
 wget -q -O hal9000.cow https://github.com/Johnson2772/scripts/tree/main/bash/preferences/HAL9000/hal9000.cow
 cp -R ~/Downloads/*9000.cow /usr/share/cowsay/cows/
